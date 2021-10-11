a168108e4799ca4cc37f0a4a50045c010829b1ad
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-10-11 12:45:12 +0100

    2 Green Results & Added Amber Results

--- spec/score_report_spec.rb
+++ spec/score_report_spec.rb
@@ -6,5 +6,18 @@ describe 'Score Report' do
     score_report.add_scores("Green")
     expect(score_report.generate_report).to eq("Test 1:\nGreen: 1\nAmber: 0\nRed: 0")
   end
+  
+  it '#generate_report returns 2 green results ' do 
+    score_report = Score_Report.new 
+    score_report.add_scores("Green, Green")
+    expect(score_report.generate_report).to eq("Test 1:\nGreen: 2\nAmber: 0\nRed: 0")
+  end
+
+  it '#generate_report returns 1 green result and 1 amber result ' do 
+    score_report = Score_Report.new 
+    score_report.add_scores("Green, Amber")
+    expect(score_report.generate_report).to eq("Test 1:\nGreen: 1\nAmber: 1\nRed: 0")
+  end
+
 end
 
